733681eb7e18a9d83f0c7cf17f955c17f7e12feb
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-08-06 00:41:35 +0300

    App and sections created

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Work from "./components/work/Work";
+import Experience  from "./components/experience/Experience";
+import Testimonials  from "./components/testimonials/Testimonials";
+import Contact  from "./components/contact/Contact";
+import "./App.scss"
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Work />
+          <Experience />
+          <Testimonials />
+          <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,9 @@
+import "./Topbar.scss"
+
+export default function topbar() {
+    return (
+        <div className= "topbar">
+            KC
+        </div>
+    )
+}
