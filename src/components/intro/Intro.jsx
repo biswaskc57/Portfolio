bb8ee06c80622b4e1f8d008e7caa1c6fcc888429
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-08-09 00:29:55 +0300

    Intro added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,25 @@
 import "./Intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/BiswasKC.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I´m </h2>
+          <h1>Biswas KC</h1>
+          <h3>
+            Software Developer<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/images.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
