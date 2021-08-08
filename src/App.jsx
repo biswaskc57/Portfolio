a264c2fffb716107e899ae0abc5f381426447aa6
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-08-08 19:51:32 +0300

    Topbar and Menu added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Work from "./components/work/Work";
-import Experience  from "./components/experience/Experience";
-import Testimonials  from "./components/testimonials/Testimonials";
-import Contact  from "./components/contact/Contact";
-import "./app.scss"
+import Menu from "./components/menu/Menu";
+import Experience from "./components/experience/Experience";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+import { useState } from "react";
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar open={menuOpen} setOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
-        <p>lamo</p>
-        <Intro/>
-        <Portfolio/>
+        <Intro />
+        <Portfolio />
         <Work />
-          <Experience />
-          <Testimonials />
-          <Contact />
+        <Experience />
+        <Testimonials />
+        <Contact />
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,32 @@
-import "./Topbar.scss"
-
-export default function topbar() {
-    return (
-        <div className= "topbar">
-           
+import "./Topbar.scss";
+import EmailIcon from "@material-ui/icons/Email";
+import PersonIcon from "@material-ui/icons/Person";
+export default function topbar({ open, setOpen }) {
+  return (
+    <div className={"topbar " + (open && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>Biswas KC</span>
+          </div>
+          <div className="itemContainer">
+            <EmailIcon className="icon" />
+            <span>biswaskc57@gmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right">
+          {" "}
+          <div className="hamburger" onClick={() => setOpen(!open)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
