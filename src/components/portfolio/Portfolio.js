78ba65baa018d98f92a5611b2cd099281a29d026
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-08-10 13:13:51 +0300

    skills added

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,5 +1,21 @@
 import "./Portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Features</li>
+        <li>Web App</li>
+        <li>Design</li>
+        <li>Front-end Apps</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Gym App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -4,29 +4,30 @@ export default function Intro() {
   return (
     <div className="skills" id="skills">
       <div className="topic">
-        <h1>Skills</h1>
+        <div class="title">
+          <span>Skills</span>
+        </div>
+        <div class="underline"></div>
       </div>
       <div className="skillcontent">
         <div className="left">
-          <div className="text">My Creative Skills & Experiences</div>
+          <div className="text">Skills & Knowledge</div>
           <div className="experience">
-            <p className="p1">Experience:-</p>
-            <p className="p2">
-              Junior web Developer at Integrify November 2019-May 2020
-            </p>
-            <p className="p3">
-              Full-Stack Development at Integrify May 2020 – October 2020
-            </p>
+            <p className="p1">Education:-</p>
+            <p className="p2">Business and IT(Haaga-Helia UAS) 2019-present</p>
+            <div className="description">
+              <p>
+                I am currently studying Business and IT in Haaga-Helia UAS. Im
+                majoring in software development and UX/UI designing.
+              </p>
+            </div>
+
+            <p className="p3">Full-Stack Development(Helsinki University) </p>
           </div>
           <div className="description">
             <p>
-              I have experience in multiple technologies and interpersonal
-              skills, such as, HTML5, CSS3, SASS, JavaScript, Typescript, React,
-              Redux, Gatsby.js, Mailchimp and have applied them while developing
-              various client-based projects. In addition to these frontend
-              skills, I have also experience with backend development skills
-              with Node.js ,data base with Mongo Db.I am also familier with
-              various Devops Tools,such as version Control Git,CI/CD(gitLab){" "}
+              I have completed full-stack course from Helsinki University with
+              react/redux at the front-end and node js at the back-end.{" "}
             </p>
           </div>
         </div>
@@ -38,77 +39,55 @@ export default function Intro() {
                 <span>HTML</span>
                 <span>90%</span>
               </div>
-              <div class="line html"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>CSS</span>
-                <span>70%</span>
+              <div class="line">
+                <div class="html"></div>
               </div>
-              <div class="line css"></div>
             </div>
+
             <div class="bars">
               <div class="info">
-                <span>SCSS</span>
-                <span>70%</span>
-              </div>
-              <div class="line mysql"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>JavaScript</span>
+                <span>CSS/SCSS</span>
                 <span>80%</span>
               </div>
-              <div class="line js"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>React.js</span>
-                <span>90%</span>
+              <div class="line">
+                <div class="css"></div>
               </div>
-              <div class="line php"></div>
             </div>
             <div class="bars">
               <div class="info">
-                <span>Redux.js</span>
+                <span>React & Redux</span>
+                <span>80%</span>
+              </div>
+              <div class="line">
+                <div class="react"></div>
+              </div>
+            </div>
+            <div class="bars">
+              <div class="info">
+                <span>Java & Springboot</span>
+                <span>80%</span>
+              </div>
+              <div class="line">
+                <div class="java"></div>
+              </div>
+            </div>
+            <div class="bars">
+              <div class="info">
+                <span>Mongo DB & Mysql</span>
                 <span>70%</span>
               </div>
-              <div class="line mysql"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>Typescript</span>
-                <span>70%</span>
+              <div class="line">
+                <div class="database"></div>
               </div>
-              <div class="line mysql"></div>
             </div>
             <div class="bars">
               <div class="info">
-                <span>Gatsby.js</span>
-                <span>70%</span>
-              </div>
-              <div class="line php"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>Node.js</span>
-                <span>70%</span>
-              </div>
-              <div class="line mysql"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>MongoDb</span>
-                <span>70%</span>
-              </div>
-              <div class="line mysql"></div>
-            </div>
-            <div class="bars">
-              <div class="info">
-                <span>Sql</span>
+                <span>AWS</span>
                 <span>60%</span>
               </div>
-              <div class="line mysql"></div>
+              <div class="line">
+                <div class="cloud"></div>
+              </div>
             </div>
           </div>
         </div>
