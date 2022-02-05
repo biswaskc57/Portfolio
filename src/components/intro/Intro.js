ca8a6ad8acd010ccb9d56024930eb3da5501fd1b
Biswas <biswas.kc@frosmo.com>
2022-02-05 22:37:28 +0200

    fixed intro page

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -16,27 +16,28 @@ export default function Intro() {
   return (
     <div className="intro" id="intro">
       <div className="left">
-        <div className="imgContainer">
-          <img src="assets/Cropped.png" alt="" />
-        </div>
-      </div>
-      <div className="right">
         <div className="wrapper">
-          <h2>Hi there, I´m </h2>
-          <h1>Biswas KC</h1>
-          <h4>
-            <a href="https://www.canva.com/design/DAEmm0R3Cjs/view" target="_blank">
-              <p> Download CV</p>
-            </a>
-          </h4>
-
+          <h2>Hi there, I'm </h2>
+          <h1>Biswas K C</h1>
           <h3>
             <span ref={text}></span>
           </h3>
+          <button className="button">
+            <a href="https://www.canva.com/design/DAEmm0R3Cjs/view" target="_blank">
+               Download CV
+            </a> 
+            </button>
         </div>
-        <a href="#portfolio">
+        <div>
+        <a href="#portfolio" className="pointer">
           <img src="assets/images.png" alt="" />
         </a>
+        </div>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <img src="assets/Cropped.png" alt="" />
+        </div>
       </div>
     </div>
   );
