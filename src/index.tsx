7574f2445d9f73d4ab4a2fcbb229ba2a5e547b01
Biswas_user <biswaskc57@gmail.com>
2024-10-26 13:06:02 +0300

    Fixed monir lint issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
