7fcf9a066d151b138ce7e97e45d167c03a2c6eae
Biswas KC <biswaskc57@gmail.com>
2024-10-28 10:23:58 +0200

    Added animation to image and text

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -2,27 +2,27 @@ import React from "react";
 import styles from "./Intro.module.scss";
 
 import avatar from "../../assets/avatar.jpg";
+import Pdf from "../../assets/resume.pdf";
 
 
 const Intro: React.FC = () => {
   return (
     <div className={styles.introContainer} >
       <div className={styles.introElements}>
-        <img className={styles.myImage} src={avatar} alt="profile" />
+        <img id ={styles.slider} className={styles.myImage} src={avatar} alt="profile" />
         <div className={styles.typewriterText}>
           <p className={styles.name}>
           Hello, <span>I&apos;m Biswas K C!</span>
           </p>
           <p className={styles.profession}>Web Developer!</p>
-          <a href={"/assets/resume.pdf"} rel="noopener noreferrer" target="_blank">
+          <a href={Pdf} rel="noopener noreferrer" target="_blank">
             <button
-              aria-label="Resume"
               title="View resume"
-              className={styles.resumeButton}
             >
-            CV
+              Resume
             </button>
           </a>
+
         </div>
       </div>
     </div>
